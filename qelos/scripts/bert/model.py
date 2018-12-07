bfb558537b07318cb17bf3b628d8f76bd5448fc3
Denis <lukovnikov@users.noreply.github.com>
2018-12-07 13:23:20 +0100

    Update model.py

--- qelos/scripts/bert/model.py
+++ qelos/scripts/bert/model.py
@@ -42,7 +42,7 @@ class AdaptedBERTEncoderSingle(torch.nn.Module):
         if self.numout >= 0:
             torch.nn.init.normal_(self.lin.weight, 0, self.bert.init_range)
             torch.nn.init.zeros_(self.lin.bias)
-        self.bert.reset_parameters()
+        #self.bert.reset_parameters()
 
     def forward(self, inpids, ret_layer_outs=False):
         """
@@ -163,8 +163,9 @@ class AdaptedBERTCompare(torch.nn.Module):
         self.right_enc = AdaptedBERTEncoderSingle(rightbert, numout=-1, oldvocab=oldvocab, specialmaps=specialmaps)
 
     def reset_parameters(self):
-        self.left_enc.reset_parameters()
-        self.right_enc.reset_parameters()
+        #self.left_enc.reset_parameters()
+        #self.right_enc.reset_parameters()
+        pass
 
     def forward(self, q, rel):
         aenc = self.left_enc(q)
